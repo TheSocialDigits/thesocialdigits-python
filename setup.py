fad71c3e33002afc7bf3dde2b4b0967248245a4c
hkb <hans.kristian.bjerregaard@gmail.com>
2011-11-16 13:01:28 +0000

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='The Social Digits Python language binding',
+      version='1.0',
+      description='Language binding for the Social Digits API for Python.',
+      author='Hans-Kristian Bjerregaard',
+      author_email='support@thesocialdigits.com',
+      url='http://www.thesocialdigits.com/documentation/language-binding/python',
+      packages=['thesocialdigits'])
